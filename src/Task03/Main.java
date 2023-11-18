4cba3fa5236dfc4c9df778d7e0d2d169163e5d64
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2023-11-18 20:51:33 +0200

    add task 03

---
+++ src/Task03/Main.java
@@ -0,0 +1,21 @@
+package Task03;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 6;
+        int sum = 0;
+        int index = 1;
+
+        while (num > 0) {
+            sum += num;
+            System.out.println(index + ") Num is " + num + ", sum is " + sum);
+            index += 1;
+            num -= 1;
+        }
+
+        System.out.println("-----------------------\n" + "Sum of numbers is " + sum);
+    }
+}
+
